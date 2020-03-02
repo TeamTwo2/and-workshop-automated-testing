e5d528b323532558b2ec3f6cff3445cc4a5381b5
Dylan Rhodius <drhodius@and.digital>
2020-03-02 14:10:13 +0000

    Add coverage report to gitignore

--- lab-2-improve-code/src/index.js
+++ lab-2-improve-code/src/index.js
@@ -3,17 +3,15 @@ module.exports = function greeting(i = 'my friend') {
     i = [i];
   }
 
-  out = 'Hello, ';
-
-  nStr = '';
-  for (j in i) {
+  let nStr = '';
+  for (let j in i) {
     nStr =
       nStr +
       i[j] +
       (parseInt(j) + 2 == i.length ? ', and ' : parseInt(j) + 1 == i.length ? '' : ', ');
   }
 
-  if (nStr.toUpperCase() === nStr) return `HELLO ${  nStr  }!`;
+  if (nStr.toUpperCase() === nStr) return `HELLO ${nStr}!`;
 
-  return `Hello, ${  nStr  }.`;
+  return `Hello, ${nStr}.`;
 };
