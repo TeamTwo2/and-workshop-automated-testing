69293909168afa72d4061d57ed59f538f3a81980
Dylan Rhodius <drhodius@and.digital>
2020-03-02 13:53:58 +0000

    Add npm run lint and jest env integration to lint config

--- lab-2-improve-code/tests.test.js
+++ lab-2-improve-code/tests.test.js
@@ -1,33 +1,33 @@
-const greeting = require("./src/index");
+const greeting = require('./src/index');
 
-describe("Greeting Test", () => {
-  describe("Requirement 1", () => {
-    it("is a func", () => {
-      expect(typeof greeting).toEqual("function");
+describe('Greeting Test', () => {
+  describe('Requirement 1', () => {
+    it('is a func', () => {
+      expect(typeof greeting).toEqual('function');
     });
   });
 
-  describe("Requirement 2", () => {
+  describe('Requirement 2', () => {
     it("shows correct out put when in put is 'test'", () => {
-      expect(greeting("test")).toEqual("Hello, test.");
+      expect(greeting('test')).toEqual('Hello, test.');
     });
   });
 
-  describe("Requirement 3", () => {
+  describe('Requirement 3', () => {
     it("shows correct out put when in put is 'test'", () => {
-      expect(greeting("test")).toEqual("Hello, test.");
+      expect(greeting('test')).toEqual('Hello, test.');
     });
   });
 
-  describe("Requirement 4", () => {
+  describe('Requirement 4', () => {
     it("shows correct out put when in put is 'test'", () => {
-      expect(greeting("test")).toEqual("Hello, test.");
+      expect(greeting('test')).toEqual('Hello, test.');
     });
   });
 
-  describe("Requirement 5", () => {
+  describe('Requirement 5', () => {
     it("shows correct out put when in put is 'test'", () => {
-      expect(greeting("test")).toEqual("Hello, test.");
+      expect(greeting('test')).toEqual('Hello, test.');
     });
   });
 });
